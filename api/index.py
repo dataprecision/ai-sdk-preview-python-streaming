675a0226af487991451d0b614d4d1aa6e5635dfc
DWAO LLP <ketavsharma@dwao.in>
2025-06-17 14:27:18 +0530

    Added OperRouter Changes

--- api/index.py
+++ api/index.py
@@ -16,7 +16,8 @@ load_dotenv(".env.local")
 app = FastAPI()
 
 client = OpenAI(
-    api_key=os.environ.get("OPENAI_API_KEY"),
+    api_key=os.environ.get("OPENROUTER_API_KEY"),
+    base_url="https://openrouter.ai/api/v1"
 )
 
 
@@ -31,29 +32,29 @@ available_tools = {
 def do_stream(messages: List[ChatCompletionMessageParam]):
     stream = client.chat.completions.create(
         messages=messages,
-        model="gpt-4o",
+        model="openai/gpt-4o-mini",
         stream=True,
-        tools=[{
-            "type": "function",
-            "function": {
-                "name": "get_current_weather",
-                "description": "Get the current weather at a location",
-                "parameters": {
-                    "type": "object",
-                    "properties": {
-                        "latitude": {
-                            "type": "number",
-                            "description": "The latitude of the location",
-                        },
-                        "longitude": {
-                            "type": "number",
-                            "description": "The longitude of the location",
-                        },
-                    },
-                    "required": ["latitude", "longitude"],
-                },
-            },
-        }]
+        # tools=[{
+        #     "type": "function",
+        #     "function": {
+        #         "name": "get_current_weather",
+        #         "description": "Get the current weather at a location",
+        #         "parameters": {
+        #             "type": "object",
+        #             "properties": {
+        #                 "latitude": {
+        #                     "type": "number",
+        #                     "description": "The latitude of the location",
+        #                 },
+        #                 "longitude": {
+        #                     "type": "number",
+        #                     "description": "The longitude of the location",
+        #                 },
+        #             },
+        #             "required": ["latitude", "longitude"],
+        #         },
+        #     },
+        # }]
     )
 
     return stream
@@ -64,29 +65,29 @@ def stream_text(messages: List[ChatCompletionMessageParam], protocol: str = 'dat
 
     stream = client.chat.completions.create(
         messages=messages,
-        model="gpt-4o",
+        model="openai/gpt-4o-mini",
         stream=True,
-        tools=[{
-            "type": "function",
-            "function": {
-                "name": "get_current_weather",
-                "description": "Get the current weather at a location",
-                "parameters": {
-                    "type": "object",
-                    "properties": {
-                        "latitude": {
-                            "type": "number",
-                            "description": "The latitude of the location",
-                        },
-                        "longitude": {
-                            "type": "number",
-                            "description": "The longitude of the location",
-                        },
-                    },
-                    "required": ["latitude", "longitude"],
-                },
-            },
-        }]
+        # tools=[{
+        #     "type": "function",
+        #     "function": {
+        #         "name": "get_current_weather",
+        #         "description": "Get the current weather at a location",
+        #         "parameters": {
+        #             "type": "object",
+        #             "properties": {
+        #                 "latitude": {
+        #                     "type": "number",
+        #                     "description": "The latitude of the location",
+        #                 },
+        #                 "longitude": {
+        #                     "type": "number",
+        #                     "description": "The longitude of the location",
+        #                 },
+        #             },
+        #             "required": ["latitude", "longitude"],
+        #         },
+        #     },
+        # }]
     )
 
     for chunk in stream:
